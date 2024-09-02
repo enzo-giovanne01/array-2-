71e692c56917544e2b1a34b09cd932364f5de841
enzo-giovanne01 <giovanneenzo01@gmail.com>
2024-09-01 23:21:01 -0300

    array-2-

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+string[] nomes = {"Eduardo", "Joao", "Adrian", "Vinicius", "Paulo"};
+WriteLine("O tamanho do array é: " + nomes.Length);
+Array.Sort(nomes);
+WriteLine("Os nomes são....");
+foreach(string nome in nomes)
+{
+    WriteLine(nome);
+}
+WriteLine();
+string novonome = "Ariel";
+List<string> adnome = new List<String>(nomes.ToList());
+adnome.Add(novonome);
+nomes = adnome.ToArray();
+WriteLine("O tamanho do array é: "+ nomes.Length);
+Array.Sort(nomes);
+WriteLine("Os nomes são......");
+foreach (var item in nomes)
+{
+    WriteLine(item);
+}
+ReadKey();
+
+
